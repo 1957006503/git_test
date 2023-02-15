b90c6ca16de0e78fd158019ec24db80ef8ec0488
Lishuo <1957006503@qq.com>
2023-02-15 21:06:21 +0800

    master commit 4

--- src/main/java/com/example/git/GitTest2.java
+++ src/main/java/com/example/git/GitTest2.java
@@ -9,5 +9,6 @@ public class GitTest2 {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("master test");
+        System.out.println("master test4");
     }
 }
