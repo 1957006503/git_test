55ad7bb53c961b51ffe860934198d9f550d5edb5
Lishuo <1957006503@qq.com>
2023-02-15 21:01:41 +0800

    master commit 3

---
+++ src/main/java/com/example/git/GitTest2.java
@@ -0,0 +1,13 @@
+package com.example.git;
+
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word! hot-fix");
+        System.out.println("hot-fix test");
+        System.out.println("master test");
+        System.out.println("master test");
+    }
+}
